a960ddd8faff842e2bc4ffca78f656af9a4622ee
Kaganucar <kagan.ucarr@gmail.com>
2026-07-03 18:43:41 +0300

    feat: add generic IRepository<T> interface

---
+++ src/FlowCore.Application/Interfaces/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using FlowCore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowCore.Application.Interfaces
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Task<T?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<List<T>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task<List<T>> FindAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken = default);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken= default); 
+        Task AddAsync(T entity, CancellationToken cancellationToken = default);
+        void Update(T entity);
+        void Remove(T entity);
+    }
+}
