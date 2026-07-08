8035b46451f29ed5a11e306287d00b537214f683
Kaganucar <kagan.ucarr@gmail.com>
2026-07-08 16:31:31 +0300

    feat: implement GenericRepository<T>

---
+++ src/FlowCore.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,55 @@
+﻿using FlowCore.Application.Interfaces;
+using FlowCore.Domain.Entities;
+using FlowCore.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowCore.Infrastructure.Repositories
+{
+    public class GenericRepository<T> : IRepository<T> where T: BaseEntity
+    {
+        protected readonly AppDbContext _context;
+        protected readonly DbSet<T> _dbSet;
+        
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task<T?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            return await _dbSet.FindAsync(new object[] { id }, cancellationToken);
+        }
+
+        public async Task<List<T>> GetAllAsync(CancellationToken cancellationToken = default)
+        {
+            return await _dbSet.AsNoTracking().ToListAsync(cancellationToken);
+        }
+        public async Task<List<T>> FindAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken = default)
+        {
+            return await _dbSet.AsNoTracking().Where(predicate).ToListAsync(cancellationToken);
+        }
+        public async Task<bool> AnyAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken = default)
+        {
+            return await _dbSet.AnyAsync(predicate, cancellationToken);
+        }
+        public async Task AddAsync(T entity, CancellationToken cancellationToken = default)
+        {
+            await _dbSet.AddAsync(entity, cancellationToken);
+        }
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+    }
+}
