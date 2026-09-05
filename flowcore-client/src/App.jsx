49471995383c408e1a99170f2d2c317410cafb8a
Kaganucar <kagan.ucarr@gmail.com>
2026-09-05 15:37:15 +0300

    feat: add category dropdown filter

--- flowcore-client/src/App.jsx
+++ flowcore-client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, use } from "react"
 import ProductList from "./ProductList"
 
 function App() {
@@ -6,6 +6,8 @@ function App() {
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(null)
     const [searchText, setSearchText] = useState('')
+    const [categories, setCategories] = useState([])
+    const [selectedCategory, setSelectedCategory] = useState('')
 
     useEffect(() => {
         fetch(`${import.meta.env.VITE_API_URL}/Product`)
@@ -25,12 +27,21 @@ function App() {
         })
     }, [])
 
+    useEffect(() => {
+        fetch(`${import.meta.env.VITE_API_URL}/Category`)
+        .then((response) => response.json())
+        .then((data) => setCategories(data))
+        .catch((err) => console.error(err))
+    }, [])
+
     if(loading) return <p>Yukleniyor...</p>
     if(error) return <p>Hata: {error}</p>
 
-    const filteredProducts = products.filter((product) =>
-        product.name.toLowerCase().includes(searchText.toLowerCase())
-    )
+    const filteredProducts = products.filter((product) =>{
+        const matchesSearch = product.name.toLowerCase().includes(searchText.toLowerCase())
+        const matchesCategory = selectedCategory === ''|| product.categoryName === selectedCategory
+        return matchesSearch && matchesCategory
+    })
 
     return(
         <div>
@@ -40,6 +51,17 @@ function App() {
             value={searchText}
             onChange={(e) => setSearchText(e.target.value)} 
             />
+            <select
+                value={selectedCategory}
+                onChange={(e) => setSelectedCategory(e.target.value)}
+            >
+                <option value="">Tum kategoriler</option>
+                {categories.map((category) => (
+                    <option key={category.id} value={category.name}>
+                        {category.name}
+                    </option>
+                ))}
+            </select>
             <ProductList products={filteredProducts} />
         </div>
     )
