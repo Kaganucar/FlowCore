4445cabac85a1b7b33ca7125b0fb148a68f3a43e
Kaganucar <kagan.ucarr@gmail.com>
2026-09-09 15:15:17 +0300

    feat: show login state and logout button in navbar

--- flowcore-client/src/Navbar.jsx
+++ flowcore-client/src/Navbar.jsx
@@ -1,13 +1,26 @@
 import { Link } from "react-router-dom";
+import { useAuth } from "./AuthContext";
 
 function Navbar() {
+    const {user, logout} = useAuth()
+
     return (
         <nav>
             <Link to="/">Urunler</Link>
             {' | '}
+            {user ? (
+                <>
+                    <span>Merhaba, {user.userName} ({user.role})</span>
+                    {'|'}
+                    <button onClick={logout}>Çıkış yap</button>
+                </>
+            ) : (
+                <>
             <Link to="/login">Giriş</Link>
             {' | '}
             <Link to="/register">Kayıt Ol</Link>
+            </>
+            )}
         </nav>
     )
 }
